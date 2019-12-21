f94ed38a70a723f8cf0c166d020f23fc2b4b2cf3
Polina Giralt <polina.giralt@gmail.com>
2019-12-21 01:16:14 -0500

    finally

--- day_12_part_2.py
+++ day_12_part_2.py
@@ -1,5 +1,4 @@
-import copy
-import itertools
+from math import gcd
 
 
 DAY_12_INPUT = [[-3, 15, -11, 0, 0, 0],
@@ -16,40 +15,49 @@ TEST_INPUTS = [([[-1, 0, 2, 0, 0, 0],
                ]
 
 
-def update_velocity(moon1, moon2):
-    for position in range(3):
-        p1 = moon1[position]
-        p2 = moon2[position]
+def update_velocity(moon1, moon2, axis):
+    p1 = moon1[axis]
+    p2 = moon2[axis]
 
-        if p1 > p2:
-            moon1[position + 3] -= 1
-            moon2[position + 3] += 1
-        elif p1 < p2:
-            moon1[position + 3] += 1
-            moon2[position + 3] -= 1
+    if p1 > p2:
+        moon1[axis + 3] -= 1
+        moon2[axis + 3] += 1
+    elif p1 < p2:
+        moon1[axis + 3] += 1
+        moon2[axis + 3] -= 1
 
 
-def update_position(moon):
-    for position in range(3):
-        moon[position] += moon[position + 3]
+def update_position(moon, axis):
+    moon[axis] += moon[axis + 3]
+
+
+def lcm(a, b):
+    """Compute the lowest common multiple of a and b."""
+    return a * b // gcd(a, b)
 
 
 def make_moves(puzzle_data):
-    starting_data = copy.deepcopy(puzzle_data)
-    i = 0
+    starting = {axis: [row[axis] for row in puzzle_data] for axis in range(3)}
+    combinations = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
+    period = {}
 
-    while True:
-        i += 1
-        for moon1, moon2 in itertools.combinations(range(4), 2):
-            update_velocity(puzzle_data[moon1], puzzle_data[moon2])
+    for axis in range(3):
+        i = 0
+        while True:
+            i += 1
 
-        for moon in range(4):
-            update_position(puzzle_data[moon])
+            for moon1, moon2 in combinations:
+                update_velocity(puzzle_data[moon1], puzzle_data[moon2], axis)
 
-        if puzzle_data == starting_data:
-            break
+            for moon in range(4):
+                update_position(puzzle_data[moon], axis)
 
-    return i
+            if (starting[axis] == [row[axis] for row in puzzle_data] and
+                    [0, 0, 0, 0] == [row[axis + 3] for row in puzzle_data]):
+                period[axis] = i
+                break
+
+    return lcm(period[0], lcm(period[1], period[2]))
 
 
 for (test_in, test_out) in TEST_INPUTS:
